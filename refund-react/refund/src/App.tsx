8750608b555bfb2f3d3eb931f2e310c8a616f2c4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-04-02 15:34:25 -0300

    Refatora App.tsx para incluir novos componentes de entrada e navegação; reorganiza a estrutura das seções

--- refund-react/refund/src/App.tsx
+++ refund-react/refund/src/App.tsx
@@ -14,107 +14,187 @@ import ReceiptIcon from "./assets/icons/Receipt.svg?react";
 import WrenchIcon from "./assets/icons/Wrench.svg?react";
 import CircleIcon from "./assets/icons/circle-notch.svg?react";
 import ButtonIcon from "./components/button-icon";
+import InputSelect from "./components/input-select";
 import InputText from "./components/input-text";
 import NavLink from "./components/navlink";
 
 function App() {
     return (
-        <div className="flex flex-col gap-10 items-center mt-10">
-            <div className="flex flex-row gap-5 items-center justify-center">
-                <Text variant="body-xs" className="text-gray-200">
-                    NOME DA SOLICITAÇÃO
+        <main className="flex flex-col gap-8 items-center py-10 px-4 w-full min-h-screen">
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    1. Tipografia
                 </Text>
-                <Text variant="body-sm" className="text-gray-200">
-                    Alimentação
+
+                <div className="flex flex-wrap gap-5 items-end">
+                    <Text variant="body-xs" className="text-gray-200">
+                        NOME DA SOLICITAÇÃO
+                    </Text>
+                    <Text variant="body-sm" className="text-gray-200">
+                        Alimentação
+                    </Text>
+                    <Text className="text-gray-200">
+                        Dados da despesa para solicitar reembolso.
+                    </Text>
+                    <Text
+                        variant="heading-sm-semibold"
+                        className="text-green-100"
+                    >
+                        Solicitações de reembolso
+                    </Text>
+                    <Text variant="heading-sm-bold" className="text-gray-100">
+                        Tamires
+                    </Text>
+                    <Text variant="heading-lg-bold" className="text-gray-100">
+                        Solicitações
+                    </Text>
+                </div>
+            </section>
+
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    2. Botões (Interações)
                 </Text>
-                <Text className="text-gray-200">
-                    Dados da despesa para solicitar reembolso.
+
+                <div className="flex flex-wrap gap-5 items-center">
+                    <Button size="sm">Confirmar</Button>
+                    <Button size="md">Nova Solicitação</Button>
+                    <Button size="lg">Enviar</Button>
+                </div>
+            </section>
+
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    2. Botões (Inativos)
                 </Text>
-                <Text variant="heading-sm-semibold" className="text-green-100">
-                    Solicitações de reembolso
+
+                <div className="flex flex-wrap gap-5 items-center">
+                    <Button disabled size="sm">
+                        Confirmar
+                    </Button>
+                    <Button disabled size="md">
+                        Nova Solicitação
+                    </Button>
+                    <Button disabled size="lg">
+                        Enviar
+                    </Button>
+                </div>
+            </section>
+
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    3. Ícones
                 </Text>
-                <Text variant="heading-sm-bold" className="text-gray-100">
-                    Tamires
+                <div className="flex flex-wrap gap-5 items-end">
+                    <Icon
+                        fill="white"
+                        className="h-6 w-6"
+                        svg={CircleIcon}
+                        animate
+                    />
+                    <Icon fill="white" className="h-6 w-6" svg={BedIcon} />
+                    <Icon
+                        fill="white"
+                        className="h-6 w-6"
+                        svg={ArrowLeftIcon}
+                    />
+                    <Icon
+                        fill="white"
+                        className="h-6 w-6"
+                        svg={ArrowRightIcon}
+                    />
+                    <Icon fill="white" className="h-6 w-6" svg={CloudIcon} />
+                    <Icon fill="white" className="h-6 w-6" svg={DesktopIcon} />
+                    <Icon
+                        fill="white"
+                        className="h-6 w-6"
+                        svg={ForkKnifeIcon}
+                    />
+                    <Icon
+                        fill="white"
+                        className="h-6 w-6"
+                        svg={MagnifyingGlassIcon}
+                    />
+                    <Icon
+                        fill="white"
+                        className="h-6 w-6"
+                        svg={PoliceCarIcon}
+                    />
+                    <Icon fill="white" className="h-6 w-6" svg={ReceiptIcon} />
+                    <Icon fill="white" className="h-6 w-6" svg={WrenchIcon} />
+                </div>
+            </section>
+
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    4. Botoes com ícone
                 </Text>
-                <Text variant="heading-lg-bold" className="text-gray-100">
-                    Solicitações
+                <div className="flex flex-wrap gap-5 items-end">
+                    <ButtonIcon
+                        ariaLabel="Teste"
+                        handling
+                        icon={MagnifyingGlassIcon}
+                    />
+                    <ButtonIcon
+                        ariaLabel="Teste"
+                        disabled
+                        icon={MagnifyingGlassIcon}
+                    />
+
+                    <ButtonIcon ariaLabel="Teste" icon={MagnifyingGlassIcon} />
+                    <ButtonIcon ariaLabel="Teste" icon={ArrowLeftIcon} />
+                    <ButtonIcon ariaLabel="Teste" icon={ArrowRightIcon} />
+                    <ButtonIcon ariaLabel="Teste" icon={CloudIcon} />
+                </div>
+            </section>
+
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    5. Inputs
                 </Text>
-            </div>
+                <div className="flex flex-wrap gap-5 items-end ">
+                    <InputText
+                        title="NOME DA SOLICITAÇÃO"
+                        placeholder="digite aqui"
+                        size="md"
+                    />
 
-            <div className="flex flex-row gap-5">
-                <Button size="sm">Confirmar</Button>
-                <Button size="md">Nova Solicitação</Button>
-                <Button size="lg">Enviar</Button>
-            </div>
+                    <InputSelect title="CATEGORIAS" />
+                </div>
+            </section>
 
-            <div className="flex flex-row gap-5">
-                <Button handling size="sm">
-                    Confirmar
-                </Button>
-                <Button disabled size="sm">
-                    Confirmar
-                </Button>
-                <Button disabled size="md">
-                    Nova Solicitação
-                </Button>
-                <Button disabled size="lg">
-                    Enviar
-                </Button>
-            </div>
-
-            <div className="flex flex-row gap-5 bg-green-100 p-6 rounded-lg">
-                <Icon
-                    fill="white"
-                    className="h-6 w-6"
-                    svg={CircleIcon}
-                    animate
-                />
-                <Icon fill="white" className="h-6 w-6" svg={BedIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={ArrowLeftIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={ArrowRightIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={CloudIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={DesktopIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={ForkKnifeIcon} />
-                <Icon
-                    fill="white"
-                    className="h-6 w-6"
-                    svg={MagnifyingGlassIcon}
-                />
-                <Icon fill="white" className="h-6 w-6" svg={PoliceCarIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={ReceiptIcon} />
-                <Icon fill="white" className="h-6 w-6" svg={WrenchIcon} />
-            </div>
-
-            <div className="flex flex-row gap-5">
-                <ButtonIcon
-                    ariaLabel="Teste"
-                    handling
-                    icon={MagnifyingGlassIcon}
-                />
-                <ButtonIcon
-                    ariaLabel="Teste"
-                    disabled
-                    icon={MagnifyingGlassIcon}
-                />
-
-                <ButtonIcon ariaLabel="Teste" icon={MagnifyingGlassIcon} />
-                <ButtonIcon ariaLabel="Teste" icon={ArrowLeftIcon} />
-                <ButtonIcon ariaLabel="Teste" icon={ArrowRightIcon} />
-                <ButtonIcon ariaLabel="Teste" icon={CloudIcon} />
-            </div>
-
-            <div className="flex gap-5">
-                <InputText
-                    title="NOME DA SOLICITAÇÃO"
-                    placeholder="digite aqui"
-                    className="w-100 h-13"
-                />
-            </div>
-
-            <div className="flex gap-5">
-                <NavLink to="/">Teste</NavLink>
-            </div>
-        </div>
+            <section className="w-full max-w-5xl flex flex-col gap-4 border border-gray-300 p-6 rounded-xl">
+                <Text
+                    variant="heading-sm-bold"
+                    className="text-green-100 border-b border-gray-300 pb-2"
+                >
+                    6. Links
+                </Text>
+                <div className="flex flex-wrap gap-5 items-end">
+                    <NavLink to="/">Teste</NavLink>
+                    <NavLink to="/Inicio">Inicio</NavLink>
+                    <NavLink to="/Componentes">Componentes</NavLink>
+                </div>
+            </section>
+        </main>
     );
 }
 
